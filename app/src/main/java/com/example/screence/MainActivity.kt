ab48bb8349c3521a1bac68c5cd3f61251d27ff8e
Clémençot Alexandre <73541197+Alexandre-C-89@users.noreply.github.com>
2025-04-07 18:31:43 +0200

    Screen created + import icon

--- app/src/main/java/com/example/screence/MainActivity.kt
+++ app/src/main/java/com/example/screence/MainActivity.kt
@@ -1,28 +1,49 @@
 package com.example.screence
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.screence.designsystem.card.AcountCard
 import com.example.screence.ui.theme.ScreenCeTheme
+import kotlin.math.asin
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ScreenCeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Home(
+                        infoClick = {},
+                        assistanceClick = {}
                     )
                 }
             }
@@ -31,17 +52,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Home(
+    infoClick: () -> Unit,
+    assistanceClick: () -> Unit,
+) {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.tour_eiffel),
+            contentDescription = "image de fond",
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column (
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    TextButton(
+                        onClick = infoClick
+                    ) {
+                        Text(
+                            text = "Infos légales",
+                            fontSize = 16.sp,
+                            color = Color.White
+                        )
+                    }
+                    TextButton(
+                        onClick = assistanceClick
+                    ) {
+                        Text(
+                            text = "Assitance",
+                            fontSize = 16.sp,
+                            color = Color.White
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_foreground),
+                    contentDescription = "logo"
+                )
+            }
+            AcountCard(
+                onClick = {},
+                text = "JC",
+                painter = painterResource(R.drawable.ic_clear),
+                name = "Jean-François Herbert",
+                number = "184635138432"
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    ScreenCeTheme {
-        Greeting("Android")
-    }
+fun HomePreview() {
+    Home(
+        infoClick = {},
+        assistanceClick = {}
+    )
 }
--- app/src/main/java/com/example/screence/designsystem/card/AcountCard.kt
+++ app/src/main/java/com/example/screence/designsystem/card/AcountCard.kt
@@ -1,25 +1,31 @@
 package com.example.screence.designsystem.card
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,29 +34,49 @@ import com.example.screence.R
 @Composable
 fun AcountCard(
     onClick: () -> Unit,
+    text: String,
     painter: Painter,
-    secondPainter: Painter,
     name: String,
     number: String
-){
+) {
     Card(
-        modifier = Modifier.width(300.dp).height(80.dp),
-        onClick = onClick
+        modifier = Modifier
+            .width(270.dp)
+            .height(80.dp),
+        onClick = onClick,
+        shape = RoundedCornerShape(8.dp)
     ) {
         Row(
-            Modifier.fillMaxSize(),
+            Modifier.fillMaxSize().padding(10.dp),
             //horizontalArrangement = Arrangement.Center
         ) {
-            Image(
-                painter = painter,
-                contentDescription = "icône"
-            )
+            Box(
+                modifier = Modifier.fillMaxHeight().padding(horizontal = 16.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    modifier = Modifier
+                        .drawBehind {
+                            drawCircle(
+                                color = Color.Gray,
+                                radius = this.size.minDimension / 2 + 20
+                            )
+                        },
+                    text = text,
+                    color = Color.Black,
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+            }
             Spacer(modifier = Modifier.width(8.dp))
-            Column (
-                modifier = Modifier.fillMaxHeight().width(120.dp),
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(120.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.Start
-            ){
+            ) {
                 Text(
                     text = name,
                     fontSize = 12.sp,
@@ -64,8 +90,8 @@ fun AcountCard(
             }
             Spacer(modifier = Modifier.width(8.dp))
             Image(
-                modifier = Modifier.size(40.dp),
-                painter = secondPainter,
+                modifier = Modifier.size(20.dp),
+                painter = painter,
                 contentDescription = "icône"
             )
         }
@@ -74,12 +100,12 @@ fun AcountCard(
 
 @PreviewScreenSizes
 @Composable
-fun AcountCardPreview(){
+fun AcountCardPreview() {
     AcountCard(
         onClick = {},
-        painter =  painterResource(R.drawable.ic_launcher_foreground),
+        text = "JC",
         name = "Jean-françois Clémençot",
         number = "5864331168",
-        secondPainter =  painterResource(R.drawable.ic_launcher_foreground)
+        painter = painterResource(R.drawable.ic_launcher_foreground)
     )
 }
